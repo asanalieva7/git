dc91872609dd57ce532aede0d25333a66d681b80
Begaiym <asanalievabegaiym530@gmail.com>
2022-04-22 00:56:09 +0600

    Выполнил первое ДЗ

---
+++ src/com/company/HomeWork1.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class HomeWork1 {
+    public static void main(String[] args){
+        task1();
+
+    }
+    private static void task1(){
+        System.out.println("answer on first question");
+    }
+}
